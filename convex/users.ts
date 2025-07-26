59e124feba532000491f50bc43df2d90c44e3ed8
Rahul Kumar <rahulrp8677@gmail.com>
2025-07-27 00:17:16 +0530

    some function are added

--- convex/users.ts
+++ convex/users.ts
@@ -1,4 +1,4 @@
-import { mutation } from "./_generated/server";
+import { mutation,query } from "./_generated/server";
 import { v } from "convex/values";
 
 export const syncUser = mutation({
@@ -25,4 +25,27 @@ export const syncUser = mutation({
 
 },
 
+});
+
+export const getUsers = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("User is not authenticated");
+
+    const users = await ctx.db.query("user").collect();
+
+    return users;
+  },
+});
+
+export const getUserByClerkId = query({
+  args: { clerkId: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("user")
+      .withIndex("by_ClarkID", (q) => q.eq("clarkID", args.clerkId))
+      .first();
+
+    return user;
+  },
 });
